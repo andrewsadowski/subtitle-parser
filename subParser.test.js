2bf2fcd5dfe72d165edcb0464d682801bc6b45c7
admin <andrew.n.sadowski@gmail.com>
2017-11-22 15:05:55 -0800

    added test.srt file and updated test.js file

--- subParser.test.js
+++ subParser.test.js
@@ -1,8 +1,9 @@
 const mocha = require('mocha');
 const expect = require('expect');
+const parser = require('subtitles-parser');
 
 describe('Parser Tests', () => {
     it('should return a matching length of the subtitle file', () => {
-        
+
     });
 });
