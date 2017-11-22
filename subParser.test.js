2e4c8afd36db013721b9aa59e25069d24501e7f1
admin <andrew.n.sadowski@gmail.com>
2017-11-22 14:51:24 -0800

    updated describe block of test file

--- subParser.test.js
+++ subParser.test.js
@@ -1,4 +1,8 @@
 const mocha = require('mocha');
 const expect = require('expect');
 
-
+describe('Parser Tests', () => {
+    it('should return a matching length of the subtitle file', () => {
+        
+    });
+});
