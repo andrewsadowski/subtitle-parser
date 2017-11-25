159bd4896f5f5199c9c4690f01431a5689ec022b
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2017-11-24 18:23:31 -0800

    added generateSubCSV export and tests type and length

--- subParser.js
+++ subParser.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const parser = require('subtitles-parser');
+const fs = require("fs");
+const parser = require("subtitles-parser");
 
-let srt = fs.readFileSync('test.srt', 'utf8');
+let srt = fs.readFileSync("test.srt", "utf8");
 
 //Stores contents of subtitle file to array//
 let data = parser.fromSrt(srt);
@@ -9,37 +9,36 @@ let data = parser.fromSrt(srt);
 let srtLength = data.length;
 
 //Loops through array of subs, formats it, and prints to console and file//
+const generateSubCSV = () => {
+  for (i = 0; i < data.length; i++) {
+    let output = 
+    `${data[i].id}; ;${data[i].startTime} --> ${data[i].endTime}; ;${data[i].text}\n`;
 
-for (i=0; i < data.length; i++) {
-    let output = `${data[i].id}; ;${data[i].startTime} --> ${data[i].endTime}; ;${data[i].text}\n`;
+    let dline = output.replace(
+        /([^0-9])\n([^$\n]*)([^\n])/, "\1 \2\3"
+    );
+    dline = output.replace(
+      /(^\n([0-9]*)\n([^a-z$]*)\n([^$]*)\n)/, "\n\1\t\2\t\3"
+    );
 
-    let dline = output.replace(/([^0-9])\n([^$\n]*)([^\n])/, '\1 \2\3');
-    dline = output.replace(/(^\n([0-9]*)\n([^a-z$]*)\n([^$]*)\n)/, '\n\1\t\2\t\3');
-    
-  
     console.log(dline);
 
-    fs.appendFile("./outputTest.txt", dline, (err) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('the file was saved');
+    fs.appendFile("./outputTest.txt", dline, err => {
+      if (err) {
+        return console.log(err);
+      }
+    //   console.log("the file was saved");
     });
 
-    fs.appendFile("./outputCSV.csv", dline, (err) => {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('the file was saved');
+    fs.appendFile("./outputCSV.csv", dline, err => {
+      if (err) {
+        return console.log(err);
+      }
+    //   console.log("the file was saved");
     });
-}
+  }
+};
 
+generateSubCSV();
 
-
-
-
-
-
-
-
-
+module.exports = { generateSubCSV };
--- subParser.test.js
+++ subParser.test.js
@@ -2,14 +2,17 @@ const mocha = require('mocha');
 const expect = require('expect');
 const parser = require('subtitles-parser');
 const fs = require('fs');
+const {generateSubCSV} = require('./subParser');
 
-let data = fs.readFileSync('test.srt', 'utf8');
-let parsedData = parser.fromSrt(data);
-console.log(parsedData);
+var data = fs.readFileSync('test.srt', 'utf8');
+var parsedData = parser.fromSrt(data);
+// console.log(Object.keys(parsedData).length);
 
-describe('Parser Tests', (done) => {
+describe('generateSubCSV Tests', (done) => {
     it('should return a matching length of the subtitle file', () => {
-        
-        
+        expect(Object.keys(parsedData).length).toBe(6);    
+    });
+    it('should be of type object', () => {
+        expect(typeof parsedData).toBe('object')
     });
 });
