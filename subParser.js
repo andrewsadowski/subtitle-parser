02b95c3cb22dd0ab37e4f6553d0c877274de223b
admin <andrew.n.sadowski@gmail.com>
2018-05-03 15:52:14 -0700

    updated generateSub2TSV function output

--- subParser.js
+++ subParser.js
@@ -1,7 +1,7 @@
-const fs = require('fs');
-const parser = require('subtitles-parser');
+const fs = require("fs");
+const parser = require("subtitles-parser");
 
-let srt = fs.readFileSync('test.srt', 'utf8');
+let srt = fs.readFileSync("5106032941001_it.srt", "utf8");
 
 //Stores contents of subtitle file to array//
 let data = parser.fromSrt(srt);
@@ -11,14 +11,19 @@ let srtLength = data.length;
 //Loops through array of subs, formats it, and prints to console and file//
 const generateSubCSV = () => {
   for (i = 0; i < data.length; i++) {
-    let output = `${data[i].id}; ;${data[i].startTime} --> ${data[i].endTime}; ;${data[i].text}\n`;
+    let output = `${data[i].id}; ;${data[i].startTime} --> ${
+      data[i].endTime
+    }; ;${data[i].text}\n`;
 
-    let dline = output.replace(/([^0-9])\n([^$\n]*)([^\n])/, '\1 \2\3');
-    dline = output.replace(/(^\n([0-9]*)\n([^a-z$]*)\n([^$]*)\n)/, '\n\1\t\2\t\3');
+    let dline = output.replace(/([^0-9])\n([^$\n]*)([^\n])/, "\1 \2\3");
+    dline = output.replace(
+      /(^\n([0-9]*)\n([^a-z$]*)\n([^$]*)\n)/,
+      "\n\1\t\2\t\3"
+    );
 
     // console.log(dline);
 
-    fs.appendFile('./outputCSV.csv', dline, err => {
+    fs.appendFile("./outputCSV.csv", dline, err => {
       if (err) {
         return console.log(err);
       }
@@ -28,15 +33,17 @@ const generateSubCSV = () => {
 
 const generateSub2TSV = () => {
   data.forEach(sub => {
-    let output = `${sub.id}\t${sub.startTime}\t${sub.endTime}\t${sub.text}\n`;
-    // console.log(output);
-    fs.appendFile('./outputTSV.tsv', output, err => {
+    let output = `${sub.id}\t${sub.startTime} --> ${sub.endTime}\t${
+      sub.text
+    }\n`;
+    console.log(output);
+    fs.appendFile("./outputTSV.tsv", output, err => {
       if (err) return;
     });
   });
 };
 
-generateSubCSV();
+// generateSubCSV();
 generateSub2TSV();
 
 module.exports = { generateSubCSV, generateSub2TSV };
--- subParser.test.js
+++ subParser.test.js
@@ -1,22 +1,22 @@
-const mocha = require('mocha');
-const expect = require('expect');
-const parser = require('subtitles-parser');
-const fs = require('fs');
-const { generateSubCSV, generateSub2TSV } = require('./subParser');
+const mocha = require("mocha");
+const expect = require("expect");
+const parser = require("subtitles-parser");
+const fs = require("fs");
+const { generateSubCSV, generateSub2TSV } = require("./subParser");
 
-var data = fs.readFileSync('test.srt', 'utf8');
+var data = fs.readFileSync("test.srt", "utf8");
 var parsedData = parser.fromSrt(data);
 
-describe('generateSubCSV Tests', done => {
-  it('should return a matching length of the subtitle file', () => {
+describe("Subtitle Parser Tests", done => {
+  it("should return a matching length of the subtitle file", () => {
     expect(Object.keys(parsedData).length).toBe(6);
   });
 
-  it('should be of type object', () => {
-    expect(typeof parsedData).toBe('object');
+  it("should be of type object", () => {
+    expect(typeof parsedData).toBe("object");
   });
 
-  it('parser.fromSrt() should contain valid subtitle indexes', function() {
+  it("parser.fromSrt() should contain valid subtitle indexes", function() {
     let dataIndexArr = [];
     let dataStartArr = [];
     let dataEndArr = [];
@@ -29,20 +29,24 @@ describe('generateSubCSV Tests', done => {
       dataTextArr.push(obj.text);
     });
 
-    expect(dataIndexArr[3]).toBe('4');
-    expect(dataStartArr[0]).toBe('00:00:00,480');
+    expect(dataIndexArr[3]).toBe("4");
+    expect(dataStartArr[0]).toBe("00:00:00,480");
     console.log({ dataIndexArr, dataStartArr, dataEndArr, dataTextArr });
     // parsedData.every(i => expect(i).to.have.all.keys('id'));
 
-    // for (let i of parsedData) {
-    //   console.log(parsedData[i]);
-    //   for (let j in parsedData[i]) {
-    //     let s = parsedData[i][j];
-    //     expect(s).to.have.property('id');
-    //     expect(s).to.have.property('startTime');
-    //     expect(s).to.have.property('endTime');
-    //     expect(s).to.have.property('text');
+    for (let i of parsedData) {
+      console.log(parsedData[i]);
+      for (let j in parsedData[i]) {
+        let s = parsedData[i][j];
+        expect(s).to.have.property('id');
+        expect(s).to.have.property('startTime');
+        expect(s).to.have.property('endTime');
+        expect(s).to.have.property('text');
     //   }
     // }
   });
+});
+
+describe("generateSubTSV tests", done => {
+  it("");
 });
